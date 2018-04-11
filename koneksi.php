b29ef50784f07586202bbc0c081953f5da62222e
mhgufron <mgufron223@gmail.com>
2018-04-11 07:33:38 +0700

    upload foto

---
+++ koneksi.php
@@ -0,0 +1,6 @@
+<?php
+$host = "localhost"; // Nama hostnya
+$user = "root"; // Username
+$pass = "toor"; // Password (Isi jika menggunakan password)
+$connect = mysqli_connect($host, $user, $pass, "ajax"); // Koneksi ke MySQL
+?>
